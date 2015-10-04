88d7ab38dd3d747475c04d90d44e80aa314e3cc6
Peter Willsey <pwillsey@gmail.com>
2015-10-04 10:20:07 -0700

    Set up example project.

--- Example/ForecastIOClient/ViewController.swift
+++ Example/ForecastIOClient/ViewController.swift
@@ -7,18 +7,52 @@
 //
 
 import UIKit
+import ForecastIOClient
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var weatherSummaryLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set forecast.io API Key
+        ForecastIOClient.apiKey = "FORECAST-IO-API-KEY"
+        // Set units the request will be returned in
+        ForecastIOClient.units = Units.Us
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Retrieve current forecast
+        ForecastIOClient.sharedInstance.forecast(-75.6046300, longitude: -26.2090000, failure: { (error) in
+            let alert: UIAlertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert)
+            let alertAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+            alert.addAction(alertAction)
+            self.presentViewController(alert, animated: true, completion: nil)
+            }) { (forecast, forecastAPICalls) -> Void in
+                if let numberOfAPICalls: Int = forecastAPICalls {
+                    print("\(numberOfAPICalls) forecastIO API calls made today!")
+                }
+                if let currentTemperature: Double = forecast.currently?.temperature {
+                    self.temperatureLabel.text = String(Int(round(currentTemperature))) + "°"
+                }
+                if let weatherSummary: String = forecast.currently?.summary {
+                    self.weatherSummaryLabel.text = weatherSummary
+                }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func forecastIOButtonTapped(sender: AnyObject) {
+        let forecastIOURL: NSURL = NSURL(string: "http://www.forecast.io")!
+        UIApplication.sharedApplication().openURL(forecastIOURL)
+    }
 }
 
