c2f00b1b3378627bd4d2297ace0d665f36809ea0
Peter Willsey <pwillsey@gmail.com>
2015-10-07 07:42:12 -0700

    Create a scheme for carthage

---
+++ Example/ForecastIOClient/ForecastIOClient.h
@@ -0,0 +1,19 @@
+//
+//  ForecastIOClient.h
+//  ForecastIOClient
+//
+//  Created by Peter Willsey on 10/7/15.
+//  Copyright © 2015 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ForecastIOClient.
+FOUNDATION_EXPORT double ForecastIOClientVersionNumber;
+
+//! Project version string for ForecastIOClient.
+FOUNDATION_EXPORT const unsigned char ForecastIOClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ForecastIOClient/PublicHeader.h>
+
+
